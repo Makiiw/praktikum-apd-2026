4251b9d9c5f8088c3c1e75069172a87cd1dba65c
Makiiw <deluxry07@gmail.com>
2026-09-22 19:38:09 +0800

    update

--- posttest/posttest-apd-2/2609106043-Muhammad_Falih_Afif_Athallah-PT-2.py
+++ posttest/posttest-apd-2/2609106043-Muhammad_Falih_Afif_Athallah-PT-2.py
@@ -4,20 +4,20 @@ makanan_3 = 19000
 makanan_4 = 20000
 makanan_5 = 21000
 makanan_6 = 22000
-
 biaya_aplikasi = 5000
+nim = 43
+kurs_euro = 20456
 
 total_harga = makanan_1 + makanan_2 + makanan_3 + makanan_4 + makanan_5 + makanan_6
+
 total_bayar = total_harga + biaya_aplikasi
 
 harga_makanan = [makanan_1, makanan_2, makanan_3, makanan_4, makanan_5, makanan_6]
 
 rata_rata = total_bayar / len(harga_makanan)
 
-nim = 43
 bolean = nim != rata_rata
 
-kurs_euro = 20456
 total_euro = total_bayar / kurs_euro
 
 print("makanan_1 : Rp" + str(makanan_1))
@@ -35,4 +35,3 @@ print("bolean : ", bolean)
 print(f"Rata-rata Harga Makanan : Rp{rata_rata:.2f}")
 print(f"Total Euro : {total_euro:.2f} €")
 print("Harga Makanan : ", (harga_makanan[-6:]))
-
